14ffb1b6e2420c0bb3f71c398e9c53611ecfa782
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-05-18 02:54:35 +0800

    Create footer.php
    Converting to php preparing for wordpress

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer class="container-fluid">
+        <!-- div for the main image to stretch s to the edge of the page-->
+
+        <div class="container">
+            <!-- div to hold the content in the middle of the page-->
+            <div class="row">
+                <!-- a row that gives us access to the BS columns-->
+                <div class="col-md-4 footercontact">
+                    <h3>Contact</h3>
+                    <p>Email: reception@museum.wa.gov</p>
+                    <p>Phone: 1300 134 081</p>
+                    <h3>Follow us</h3>
+                    <img src="images/bestinsta_1.jpg" alt="instagram icon">
+                    <img src="images/twitter.jpg" alt="twitter icon">
+                    <img src="images/facebook_icon.jpg" alt="facebook icon">
+                    <img src="images/yout.jpg" alt="youtube icon">
+                </div>
+                <div class="col-md-4 footeroptions">
+                    <p><span class="westernaustralianmuseum">Western Australian Museum</span></p>
+                    <p>Membership</p>
+                    <p>Donate</p>
+                    <p>About</p>
+                </div><!-- row-->
+                <div class="col-md-4 footerlocation">
+                    <h3>Location & Hours</h3>
+                    <!-- should the below be so many paragraphs or smarter way of doing this? -->
+                    <ul class="list-inline justify-content-center">
+                        <li class="list-item">47 Cliff Street</li>
+                        <li class="list-item">Fremantle, WA</li>
+                        <li class="list-item">6160</li>
+                        <li class="list-item">Open Daily:</li>
+                        <li class="list-item">0930Am to 0500Pm</li>
+                    </ul>
+                </div>
+            </div><!-- container -->
+        </div><!-- container-fluid-->
+
+    </footer><?php wp_footer(); ?></body>
+
+</html>
