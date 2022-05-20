238f2fe7571d5a48beac84e895b7892726d6c57d
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-05-21 05:21:40 +0800

    Create home-page.php
    Adding Batavia homepage

---
+++ home-page.php
@@ -0,0 +1,65 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+    ?><?php get_header();?>   <!--  Tells WordPress to include header.php --><section class="container-fluid barbarismbg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 order-sm-1">
+                    <img class="barbarism_image" src="http://206.189.45.97/~mesh17/wp-content/themes/batavia_shipwreck/images/skeleton.jpg" alt="skeletal remains">
+                </div>
+                <div class="col-md-6 order-sm-12">
+                <h2><?php the_field('barbarismtitle'); ?></h2><!--  custom field for the aboutme content-->
+                    <p>After <span class="thebatavia">The Batavia</span> was wrecked, the survivors were left to the
+                        dictatorship of <span class="jeronimuscornelisz">Jeronimus
+                            Cornelisz</span>,
+                        where
+                        125 men, women and children were raped and murdered.</p>
+
+                    <a class="btn exhibit btn-primary" href="#" role="button">Exhibit&#8594;</a>
+                </div>
+
+            </div> <!-- row -->
+        </div> <!-- container -->
+
+
+    </section>
+    <section class="container-fluid reignbg ">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-6">
+                    <h2><?php the_field('reigntitle'); ?></h2>
+                    <p>Cornelisz first ordered all weapons to be handed over to his men and then the slaughter of any
+                        disloyal
+                        survivors by violently slitting their throats or drowning them. Visit the <span
+                            class="maritimemuseumperth">Maritime museum Perth</span>
+                        to see this
+                        riveting exhibit.</p>
+                    <a class="btn learn btn-primary" href="#" role="button">Learn more&#8594;</a>
+                </div>
+                <div class="col-md-6">
+                    <img class="reignimage" src="http://206.189.45.97/~mesh17/wp-content/themes/batavia_shipwreck/images/shipreplica.jpg" alt="replica of the ship">
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+    <section class="container-fluid reckoningbg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img class="reckoningimage" src="http://206.189.45.97/~mesh17/wp-content/themes/batavia_shipwreck/images/shipwreck.jpg" alt="part of the batavia">
+                </div> <!-- row -->
+                <div class="col-md-6">
+                    <h2><?php the_field('reckoningtitle'); ?></h2>
+                    <p>After the remaining survivors were rescued, Cornelisz had both his hands cut off and his men had
+                        their right
+                        hands cut off before being hung for punishment. By becoming a member you are open to more
+                        discounts at the
+                        <span class="fremantlemaritimemuseum">Fremantle Maritime Museum.</span>
+                    </p><a class="btn member btn-primary" href="#" role="button">Become a member&#8594;</a>
+                </div>
+            </div>
+        </div> <!-- container -->
+
+    </section><?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
