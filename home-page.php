8b4672a4e3cd02a76b5291be0345335260604873
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-05-21 06:33:54 +0800

    Update home-page.php
    Adding custom fields

--- home-page.php
+++ home-page.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 order-sm-1">
-                    <img class="barbarism_image" src="http://206.189.45.97/~mesh17/wp-content/themes/batavia_shipwreck/images/skeleton.jpg" alt="skeletal remains">
+                    <img class="barbarism_image" src="<?php the_field('barbarismimage');?>" alt="skeletal remains">
                 </div>
                 <div class="col-md-6 order-sm-12">
                 <h2><?php the_field('barbarismtitle'); ?></h2><!--  custom field for the aboutme content-->
@@ -16,7 +16,7 @@
                         where
                         125 men, women and children were raped and murdered.</p>
 
-                    <a class="btn exhibit btn-primary" href="#" role="button">Exhibit&#8594;</a>
+                    <a href="<?php the_field('exhibitlink'); ?>"class="btn exhibit btn-primary" href="#" role="button">Exhibit&#8594;</a>
                 </div>
 
             </div> <!-- row -->
@@ -39,7 +39,7 @@
                     <a class="btn learn btn-primary" href="#" role="button">Learn more&#8594;</a>
                 </div>
                 <div class="col-md-6">
-                    <img class="reignimage" src="http://206.189.45.97/~mesh17/wp-content/themes/batavia_shipwreck/images/shipreplica.jpg" alt="replica of the ship">
+                    <img class="reignimage" src="<?php the_field('reignimage');?>" alt="replica of the ship">
                 </div>
             </div> <!-- row -->
         </div> <!-- container -->
@@ -48,7 +48,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <img class="reckoningimage" src="http://206.189.45.97/~mesh17/wp-content/themes/batavia_shipwreck/images/shipwreck.jpg" alt="part of the batavia">
+                    <img class="reckoningimage" src="<?php the_field('reckoningimage');?>" alt="part of the batavia">
                 </div> <!-- row -->
                 <div class="col-md-6">
                     <h2><?php the_field('reckoningtitle'); ?></h2>
