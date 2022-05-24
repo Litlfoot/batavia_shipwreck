819ecf5c207433e8f7d2c08adfaa086d793b140d
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-05-25 02:08:08 +0800

    Create signup-page.php
    I think I need two more pages as a template

---
+++ signup-page.php
@@ -0,0 +1,8 @@
+<?php
+    /*
+    Template Name: Signup page
+    */
+    ?>
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
