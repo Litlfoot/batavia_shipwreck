88836452f07f3700bb0006bc80ffd949e5c8b27f
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-05-25 02:08:01 +0800

    Create about-page.php
    I think I need two more pages as a template

---
+++ about-page.php
@@ -0,0 +1,9 @@
+<?php
+    /*
+    Template Name: About page
+    */
+    ?>
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+
+
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
