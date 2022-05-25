1a80d9f77a04dce6b4d5f0ceb46f378361d6e1d2
Litlfoot <100711572+Litlfoot@users.noreply.github.com>
2022-05-26 00:05:50 +0800

    Update about-page.php
    Trying to add the loop to my post page

--- about-page.php
+++ about-page.php
@@ -3,7 +3,45 @@
     Template Name: About page
     */
     ?>
-<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+<?php require_once('about_header.php'); ?><div class="container-fluid maincontainer">
+    <div class="container">
+        <div class="row">
+            <section class="maintext">              
+            <?php 
+$args = array(
+  'category_name' =>  'About page',
+ // 'posts_per_page'    =>   1
+);
+// the query
+$the_query = new WP_Query( $args ); ?>
+ 
+<?php if ( $the_query->have_posts() ) : ?>
+ 
+    <!-- pagination here -->
+ 
+    <!-- the loop -->
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+    <h2><?php the_title(); ?></h2> 
+    <div>
+<?php
+                if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                    the_post_thumbnail('thumbnail');
+                } ?>
+<p class="about-text"><?php the_excerpt(); ?></p></div>
+    <a class="readmore" href="<?php the_permalink(); ?>"> CONTINUE READING</a>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
+ 
+    <!-- pagination here -->
+ 
+    <?php wp_reset_postdata(); ?>
+ 
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 
-
-<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
+</section>
+           
+     </div>
+    </div><!-- container  -->
+  </div>  <!-- container fluid --><?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
